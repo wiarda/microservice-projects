0112cf3bc5218c3b364ec17a835307c0a939213f
wiarda <wiarda@gmail.com>
2018-07-03 10:29:41 +0800

    -stricter url validation

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,7 +1,7 @@
 
 export function isValidUrl(str) {
     console.log("validating url",str)
-    let re = /^(?:https?:\/{2}|www.)?(?:\w+\.)+[a-z]{2,}(?:\/[\w]+)*(?:\?[;&a-z\d%_\.~+=-]+)?/i
+    let re = /^(?:https?:\/{2}|www.)?(?:\w+\.)+[a-z]{2,}((?:\/[\w]+)*(?:\?[;&a-z\d%_\.~+=-]+)?)?$/i
  
     if (re.test(str)) return true
     else return false
