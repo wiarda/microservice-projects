9d1cdffe58142c56c476af1c3815fc6859ecd974
wiarda <wiarda@gmail.com>
2018-07-05 11:58:02 +0800

    -improved template for ssr

--- components/pageTemplate.js
+++ components/pageTemplate.js
@@ -1,4 +1,12 @@
-export default function pageTemplate(title=null,content="",scripts=""){
+export default function pageTemplate({
+        title=""
+        ,content=""
+        ,scriptsArr=[]
+    }){
+    let scripts = scriptsArr.reduce( (acc,curr)=>{
+        return acc + `<script type="text/javascript" src="${curr}" async></script>`
+    },"")
+    console.log("scripts",scripts)
 
     let boilerplate =  
         `
@@ -6,15 +14,12 @@ export default function pageTemplate(title=null,content="",scripts=""){
         <html lang="en">
             <head>
                 <meta charset="utf-8">
-                <title> ${title} </title>
+                <title>${title}</title>
                 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+                ${scripts}
             </head>
-            
             <body>
-                <div id="root">
-                    ${content}
-                </div>
-                    ${scripts}
+                <div id="root">${content?content:null}</div>
             </body>
         </html>
         `
