0ee61c8c1831f4ed468ce9173db9fa3a55f74dd2
wiarda <wiarda@gmail.com>
2018-07-24 02:53:36 +0800

    -updated pageTemplate to accept server generated redux state

--- components/pageTemplate.js
+++ components/pageTemplate.js
@@ -3,15 +3,20 @@ export default function pageTemplate({
         ,content=""
         ,scriptsArr=[]
         ,stylesArr=[]
+        ,initialState=null
     }){
     
         let scripts = scriptsArr.reduce( (acc,cur)=>{
             return acc + `<script type="text/javascript" src="${cur}" async></script>`
-        },"")
+        },"");
 
         let styles = stylesArr.reduce( (acc,cur)=>{
             return acc + `<link rel="StyleSheet" href="${cur}" type="text/css"/>`
-        },"")
+        },"");
+
+        let state = initialState ? 
+        `<script>window.__STATE=${JSON.stringify(initialState)}</script>` :
+        "";
 
     console.log("scripts",scripts)
 
@@ -24,10 +29,11 @@ export default function pageTemplate({
                 <title>${title}</title>
                 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
                 ${styles}
-                ${scripts}
             </head>
             <body>
                 <div id="root">${content?content:null}</div>
+                ${state}    
+                ${scripts}
             </body>
         </html>
         `
