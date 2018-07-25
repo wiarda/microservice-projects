78152ed2f29d488dbd5201d0e2147f221e1ac602
wiarda <wiarda@gmail.com>
2018-07-26 02:27:56 +0800

    -added local account signup

---
+++ app/issueTracker/controllers/signupController.js
@@ -0,0 +1,34 @@
+import Users from '../models/Users';
+
+export function signup(req,res){
+    let {username, password, email} = req.body;
+    console.log("signup request", req.body)
+
+    let user = new Users({
+        username
+    });
+
+    user.setPassword(password);
+
+    let newAccount = user.save(function(err,user){
+        if (err) {
+            let message = handleMongoErr(err);
+            console.log("login error", err.message)
+            res.json({type: "signupErr", message})
+        }
+        else {
+            console.log("account creation successful");
+            res.json({type:"signupSuccess", message:"You've signed up!"})
+        }
+    })    
+};
+
+function handleMongoErr(err){
+    switch (err.code){
+        case "11000":
+            return "This account name is already taken, please choose another."
+        
+        default:
+            return "There was an error signing up, please try again."
+    }
+}
